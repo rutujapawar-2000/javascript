5a1c200274f060c213161578b7738878210d31e8
Rutuja Pawar <rutujapawar200021@gmail.com>
2025-05-06 05:57:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -105,4 +105,26 @@ else {
 	alert("Your number is not divisible by 2 and 3")
 }
 // Problem 4: Write a JS program to find whether a number is divisible by either 2 or 3
+let num = prompt("What is your age?")
+num = Number.parseInt(num)
+if (num % 2 == 0) {
+console.log("Your number is divisible by 2")
+} if (num % 3 == 0) {
+console.log("Your number is divisible by 3")
+}
+// Problem 5: Print "You can Drive" or "You cannot Drive" based on age being greater than 18 using ternary operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
