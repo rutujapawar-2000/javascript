b3b5320d393b8f736dea51e8870e8ded558b872f
Rutuja Pawar <rutujapawar200021@gmail.com>
2025-05-06 09:12:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -119,6 +119,14 @@ console.log(a)
 
 // Loops
 // For loop
+// Program to add first n Natural Numbers
+let sum = 0
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n)
+for (let i = 0; i < n; i++){
+sum += (i + 1)
+}
+console.log("Sum of first " + n + " natural numbers is " + sum)
 
 
 
